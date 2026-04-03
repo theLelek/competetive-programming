752b4b3af6d6215a3afe8ddee0ed42e17e39034d
leon heschl <heschl.leon@gmail.com>
2026-04-03 14:51:15 +0200

    did subarray sum 1 2. time

--- cses/sorting_and_searching/subarray_sum.cpp
+++ cses/sorting_and_searching/subarray_sum.cpp
@@ -11,7 +11,19 @@ int main() {
         cin >> current;
         numbers.push_back(current);
     }
-    for (int l )
-
+    int ans = 0;
+    int currentSum = 0;
+    int j = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+        while (j < numbers.size() && currentSum < x) {
+            currentSum += numbers[j];
+            j++;
+        }
+        if (currentSum == x) {
+            ans++;
+        }
+        currentSum -= numbers[i];
+    }
+    cout << ans;
     return 0;
 }
