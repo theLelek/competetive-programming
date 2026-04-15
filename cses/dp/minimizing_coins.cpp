5637d7bb125b40ec72a944cbe74d182086e8f9a4
leon heschl <heschl.leon@gmail.com>
2026-04-15 20:10:27 +0200

    added bottom up approach to minimizing coins

--- cses/dp/minimizing_coins.cpp
+++ cses/dp/minimizing_coins.cpp
@@ -28,19 +28,36 @@ int solveRecursively(int currentTargetSum) {
     return foo;
 }
 
+int solveIteratively(int targetSum) {
+    dp.at(0) = 0;
+    for (int i = 1; i <= targetSum; i++) {
+        // needed value = i
+        int best = targetSum + 1;
+        for (int coin : coins) {
+            if (i - coin < 0) {
+                continue;
+            }
+            int current = 1 + dp.at(i - coin);
+            best = min(best, current);
+        }
+        dp.at(i) = best;
+    }
+    return dp.at(targetSum) > targetSum ? -1 : dp.at(targetSum);
+}
+
 int main() {
     int n, x; cin >> n; cin >> x;
     for (int i = 0; i < n; i++) {
         int current; cin >> current;
         coins.push_back(current);
     }
-    dp.resize(x + 5, -1);
-    int out =  solveRecursively(x);
-
-    if (out == INT_MAX / 2) {
-        cout << -1;
-    } else {
-        cout << out;
-    }
+    dp.resize(x + 5, x + 1);
+    int out = solveIteratively(x);
+    cout << out;
     return 0;
-}
+    }
+
+/* remember for future:
+ * try to avoid INT_MAX overflows happen
+ * when chaning from INT_MAX to INT_MAX / 2 also change the if condition in the main function
+*/
