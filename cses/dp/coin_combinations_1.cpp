a0d0e09a11a42f5c56c238fd7be32367e46c4eaa
leon heschl <heschl.leon@gmail.com>
2026-04-19 20:35:59 +0200

    finished coin_combinations_1 bottom up

--- cses/dp/coin_combinations_1.cpp
+++ cses/dp/coin_combinations_1.cpp
@@ -19,12 +19,28 @@ int solveRecursively(int targetSum) {
 
     int count = 0;
     for (int i = 0; i < numbers.size(); i++) {
-        int foo = solveRecursively(targetSum - numbers.at(i)) % 1000000007;
-        count = (foo != -1) ? count + foo : count;
+        count += solveRecursively(targetSum - numbers.at(i)) % 1000000007;
         count = count % 1000000007;
     }
     dp.at(targetSum) = count;
     return count;
+}
+
+int solveIteratively(int targetSum) {
+    dp.at(0) = 1;
+    for (int i = 1; i <= targetSum; i++) {
+        int count = 0;
+        for (int j = 0; j < numbers.size(); j++) {
+            int prev = i - numbers.at(j);
+            if (prev < 0 || dp.at(prev) == -1) {
+                continue;
+            }
+            count += dp.at(prev);
+            count %= 1000000007;
+        }
+        dp.at(i) = count;
+    }
+    return dp.at(targetSum);
 }
 
 int main() {
@@ -35,6 +51,6 @@ int main() {
     }
 
     dp.resize(x + 5, -1);
-    cout << solveRecursively(x);
+    cout << solveIteratively(x);
     return 0;
 }
