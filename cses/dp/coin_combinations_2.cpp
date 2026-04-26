debcd13f33b0188eef8f1f1ca6d7104bcdc9dc0a
leon heschl <heschl.leon@gmail.com>
2026-04-26 10:00:27 +0200

    finished coin combinations 2 :)

--- cses/dp/coin_combinations_2.cpp
+++ cses/dp/coin_combinations_2.cpp
@@ -3,27 +3,49 @@
 
 using namespace std;
 
-vector<vector<int>> dp; // index = coin sum | value = how many combinations
+vector<long long> dp; // index = coin sum | value = how many combinations
 vector<int> numbers;
 
-int solveRecursively(int targetSum, int idx) {
-    if (targetSum == 0) {
-        return 1;
-    }
-    if (targetSum < 0) {
-        return 0;
-    }
-    if (dp.at(targetSum).at(idx) != -1) {
-        return dp.at(targetSum).at(idx);
-    }
+// int solveRecursively(int targetSum, int idx) {
+//     if (targetSum == 0) {
+//         return 1;
+//     }
+//     if (targetSum < 0) {
+//         return 0;
+//     }
+//     if (idx == numbers.size()) {
+//         return 0;
+//     }
+//     if (dp.at(targetSum).at(idx) != -1) {
+//         return dp.at(targetSum).at(idx);
+//     }
+//
+//     int count = 0;
+//     for (int i = idx; i < numbers.size(); i++) {
+//         count += solveRecursively(targetSum - numbers.at(i), i) % 1000000007;
+//         count = count % 1000000007;
+//     }
+//     dp.at(targetSum).at(idx) = count;
+//     return count;
+// }
 
-    int count = 0;
-    for (int i = idx; i < numbers.size(); i++) {
-        count += solveRecursively(targetSum - numbers.at(i), i) % 1000000007;
-        count = count % 1000000007;
+int solveIteratively(int targetSum) {
+    dp.assign(targetSum + 5, 0);
+    dp.at(0) = 1;
+    for (int i = 0; i < numbers.size(); i++) {
+        // index
+        for (int j = 1; j <= targetSum; j++) {
+            // targetSum
+            int currentNumber = numbers.at(i);
+            int prev = j - currentNumber;
+            if (prev < 0) {
+                continue;
+            }
+            dp.at(j) += dp.at(prev);
+            dp.at(j) %= 1000000007;
+        }
     }
-    dp.at(targetSum).at(idx) = count;
-    return count;
+    return dp.at(targetSum);
 }
 
 
@@ -34,7 +56,7 @@ int main() {
         numbers.push_back(c);
     }
 
-    dp.resize(x + 5, vector<int>(n + 5, -1));
-    cout << solveRecursively(x, 0);
+//    dp.resize(x + 5, vector<int>(n + 5, -1));
+    cout << solveIteratively(x);
     return 0;
 }
