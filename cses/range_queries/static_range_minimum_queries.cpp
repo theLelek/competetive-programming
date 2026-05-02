b211182921885641fcc34b30a8cc4e340bc1153c
leon heschl <heschl.leon@gmail.com>
2026-05-02 22:29:43 +0200

    improved sparse table

--- cses/range_queries/static_range_minimum_queries.cpp
+++ cses/range_queries/static_range_minimum_queries.cpp
@@ -16,32 +16,32 @@ int main() {
     // preprocessing
     vector<vector<int>> sparseTable;
     for (int i = 0; pow(2, i) <= numbers.size(); i++) {
-        sparseTable.push_back(vector<int>(numbers.size(), INT_MAX));
-        int interval = pow(2, i);
-        int priorInterval = interval / 2;
+        sparseTable.push_back({});
+        int powerOf2 = pow(2, i);
+        int prevPowerOf2 = pow(2, i - 1);
         for (int j = 0; j + pow(2, i) - 1 < numbers.size(); j++) {
             if (i == 0) {
-                sparseTable.at(i).at(j) = numbers.at(j);
+                sparseTable.at(i).push_back(numbers.at(j));
                 continue;
             }
-            int c = min(sparseTable.at(i - 1).at(j), sparseTable.at(i - 1).at(j + priorInterval));
-            sparseTable.at(i).at(j) = (c);
+            int colum1 = j;
+            int colum2 = j + prevPowerOf2;
+            int m = min(sparseTable.at(i - 1).at(colum1), sparseTable.at(i - 1).at(colum2));
+            sparseTable.at(i).push_back(m);
         }
     }
 
-    // answering queries
-    // for minimum queries overlaps dont matter -> thus constant answering is possible
-    // non overlapping answering takes log2(n)
+    // int answering
     while (q--) {
-        int from; cin >> from; int to; cin >> to; from--; to--;
-        int queryLength = to - from + 1;
-        int prevPowerOf2 = (int) log2(queryLength);
+        int from; int to; cin >> from; cin >> to; from--; to--;
 
-        int q1From = from;
-        int q2From = to - pow(2, prevPowerOf2) + 1;
-        cout << min(sparseTable.at(prevPowerOf2).at(q1From), sparseTable.at(prevPowerOf2).at(q2From)) << "\n";
+        int qLength = to - from + 1;
+        int greatestPowerOf2 = (int) log2(qLength);
+        int ans1 = sparseTable.at(greatestPowerOf2).at(from);
+        int ans2 = sparseTable.at(greatestPowerOf2).at(to - pow(2, greatestPowerOf2) + 1);
+        cout << min(ans1, ans2) << "\n";
     }
-
 
     return 0;
 }
+
