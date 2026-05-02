3181f17a0f022378f4525cdb77e69dfef34ded13
leon heschl <heschl.leon@gmail.com>
2026-05-02 22:48:19 +0200

    improved sparse table

--- cses/range_queries/static_range_minimum_queries.cpp
+++ cses/range_queries/static_range_minimum_queries.cpp
@@ -1,47 +1,48 @@
+
+
 #include <cmath>
 #include <iostream>
-#include <limits.h>
 #include <vector>
-
 using namespace std;
 
 int main() {
-    int n; int q; cin >> n; cin >> q;
+    int n; cin >> n; int x; cin >> x;
     vector<int> numbers;
     while (n--) {
         int c; cin >> c;
         numbers.push_back(c);
     }
 
-    // preprocessing
+    // building
     vector<vector<int>> sparseTable;
     for (int i = 0; pow(2, i) <= numbers.size(); i++) {
         sparseTable.push_back({});
-        int powerOf2 = pow(2, i);
-        int prevPowerOf2 = pow(2, i - 1);
-        for (int j = 0; j + pow(2, i) - 1 < numbers.size(); j++) {
+        for (int j = 0; j + pow(2, i) - 1 < numbers.size(); j++) { // todo
             if (i == 0) {
                 sparseTable.at(i).push_back(numbers.at(j));
                 continue;
             }
             int colum1 = j;
-            int colum2 = j + prevPowerOf2;
+            int colum2 = j + pow(2, i - 1);
             int m = min(sparseTable.at(i - 1).at(colum1), sparseTable.at(i - 1).at(colum2));
             sparseTable.at(i).push_back(m);
         }
     }
 
-    // int answering
-    while (q--) {
+    // processing
+    while (x--) {
         int from; int to; cin >> from; cin >> to; from--; to--;
 
         int qLength = to - from + 1;
-        int greatestPowerOf2 = (int) log2(qLength);
-        int ans1 = sparseTable.at(greatestPowerOf2).at(from);
-        int ans2 = sparseTable.at(greatestPowerOf2).at(to - pow(2, greatestPowerOf2) + 1);
-        cout << min(ans1, ans2) << "\n";
-    }
+        int greatedPowerOf2 = (int) log2(qLength);
 
+        int colum1 = from;
+        int colum2 = to - pow(2, greatedPowerOf2) + 1;
+        int ans = min(sparseTable.at(greatedPowerOf2).at(colum1), sparseTable.at(greatedPowerOf2).at(colum2));
+        cout << ans << "\n";
+    }
     return 0;
 }
 
+
+// todo building different than precessing?
