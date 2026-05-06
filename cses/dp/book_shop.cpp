8778aac5109495ac341a23121d199454fcdf2d9f
leon heschl <heschl.leon@gmail.com>
2026-05-06 23:58:50 +0200

    finished top down book shop

--- cses/dp/book_shop.cpp
+++ cses/dp/book_shop.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits.h>
 #include <vector>
 
 using namespace std;
@@ -10,24 +11,25 @@ vector<vector<int>> dp;
 
 int solveRecursively(int currentPrice, int idx) {
     if (currentPrice > maxPrice) {
-        return 0;
+        return INT_MIN;
     }
     if (idx == prices.size()) {
         return 0;
     }
-//  if (dp.at(idx).at(currentPrice) != -1) {
-//      return dp.at(idx).at(currentPrice);
-//  }
+    if (dp.at(idx).at(currentPrice) != -1) {
+        return dp.at(idx).at(currentPrice);
+    }
 
     int ans1 = solveRecursively(currentPrice + prices.at(idx), idx + 1) + pages.at(idx);
     int ans2 = solveRecursively(currentPrice, idx + 1);
+    dp.at(idx).at(currentPrice) = max(ans1, ans2);
     return max(ans1, ans2);
 }
 
 int main() {
     int n; cin >> n; int x; cin >> x;
     maxPrice = x;
-    dp.resize(n + 5, vector<int>(1005, -1));
+    dp.resize(n + 5, vector<int>(x + 5, -1));
     for (int i = 0; i < n; i++) {
         int c; cin >> c;
         prices.push_back(c);
