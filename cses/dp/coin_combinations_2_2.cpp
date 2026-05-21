a8062ebae2d442752eb5749abb048c0605b5856d
leon heschl <heschl.leon@gmail.com>
2026-05-21 22:10:48 +0200

    finished coin combi 2 second time

--- cses/dp/coin_combinations_2_2.cpp
+++ cses/dp/coin_combinations_2_2.cpp
@@ -29,22 +29,21 @@ int solveRecursively(int idx, int targetSum) {
 
 int solveIteratively(int x) {
     for (int i = 0; i < numbers.size(); i++) {
-        int best = INT_MAX;
+        dp.at(i).at(0) = 1;
         for (int j = 1; j <= x; j++) {
-            if (i - j >= 0) {
-
-
+            int ans = (i > 0) ? dp.at(i - 1).at(j): 0;
+            if (j - numbers.at(i) >= 0) {
+                ans += dp.at(i).at(j - numbers.at(i));
             }
-            int ans1 = dp.at(i).at(j)
-
-
+            dp.at(i).at(j) = ans % 1000000007;
         }
     }
+    return dp.at(numbers.size() - 1).at(x);
 }
 
 int main() {
     int n, x; cin >> n; cin >> x;
-    dp.resize(n + 5, vector<int>(x + 5, 0)); // change for recursive
+    dp.resize(n + 5, vector<int>(x + 5, 1)); // change for recursive
     for (int i = 0; i < n; i++) {
         int c; cin >> c;
         numbers.push_back(c);
