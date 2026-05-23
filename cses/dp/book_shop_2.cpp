501cbac2c0c447fa73e4b1d41a00cd84015d4b5e
Leon Heschl <heschl.leon@gmail.com>
2026-05-23 23:50:38 +0200

    finished book shop 2 recursively

--- cses/dp/book_shop_2.cpp
+++ cses/dp/book_shop_2.cpp
@@ -7,22 +7,25 @@ using namespace std;
 
 vector<int> prices;
 vector<int> pages;
+vector<vector<int>> dp;
 
 int solveRecursively(int idx, int money) {
     if (money < 0) {
-        return INT_MIN;
+        return INT_MIN / 2;
     }
     if (idx == prices.size()) {
         return 0;
     }
+    if (dp.at(idx).at(money) != -1) {
+        return dp.at(idx).at(money);
+    }
 
-    int ans = INT_MIN;
+    int ans = solveRecursively(idx + 1, money);
     for (int i = idx; i < prices.size(); i++) {
-        int foo = solveRecursively(i, money - prices.at(i));
-
-
+        int foo = solveRecursively(i + 1, money - prices.at(i));
         ans = max(ans, pages.at(i) + foo);
     }
+    dp.at(idx).at(money) = ans;
     return ans;
 }
 
@@ -36,6 +39,7 @@ int main() {
         int c; cin >> c;
         pages.push_back(c);
     }
-    cout << solveRecursively(x);
+    dp.resize(n + 5, vector<int>(x + 5, -1));
+    cout << solveRecursively(0, x);;
     return 0;
 }
