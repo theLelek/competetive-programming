74cca80370e197c964e5ddca0b926a8ff94e92b2
leon heschl <heschl.leon@gmail.com>
2026-05-27 12:47:20 +0200

    added dp

--- cses/dp/array_description.cpp
+++ cses/dp/array_description.cpp
@@ -6,23 +6,30 @@ using namespace std;
 
 vector<int> numbers;
 int m;
+vector<vector<int>> dp;
 
 int solveRecursively(int idx, int prevValue) {
     if (idx == numbers.size()) {
         return 1;
+    }
+    if (dp.at(idx).at(prevValue) != -1) {
+        return dp.at(idx).at(prevValue);
     }
     int ans = 0;
     if (numbers.at(idx) == 0) {
         for (int i = 1; i <= m; i++) {
            if (abs(prevValue - i) <= 1 || idx == 0) {
                ans += solveRecursively(idx + 1, i);
+               ans %= 1000000007;
            }
         }
     } else if (abs(prevValue - numbers.at(idx)) <= 1) {
         ans += solveRecursively(idx + 1,  numbers.at(idx));
+        ans %= 1000000007;
     } else {
         return 0;
     }
+    dp.at(idx).at(prevValue) = ans;
     return ans;
 }
 
@@ -35,6 +42,7 @@ int main() {
         cin >> c;
         numbers.push_back(c);
     }
+    dp.resize(n + 5, vector<int>(m + 5, -1));
     cout << solveRecursively(0, numbers.at(0));
     return 0;
 }
