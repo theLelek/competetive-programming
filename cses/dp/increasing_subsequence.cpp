49a2cae07d3039766f65f19694de9ae0a271e040
leon heschl <heschl.leon@gmail.com>
2026-06-29 01:23:35 +0200

    sync

--- cses/dp/increasing_subsequence.cpp
+++ cses/dp/increasing_subsequence.cpp
@@ -7,26 +7,42 @@ using namespace std;
 int n;
 vector<int> numbers;
 
-vector<vector<int>> dp;
+vector<int> dp;
 
-int solveRecursively(int idx, int prevIndex) {
-    if (idx == n) {
-        return 0;
-    }
-    if (prevIndex != -1 && dp.at(idx).at(prevIndex) != -1) {
-        return dp.at(idx).at(prevIndex);
+// int solveRecursively(int idx, int prevIndex) {
+//     if (idx == n) {
+//         return 0;
+//     }
+//     if (prevIndex != -1 && dp.at(idx).at(prevIndex) != -1) {
+//         return dp.at(idx).at(prevIndex);
+//     }
+//
+//     int foo1 = solveRecursively(idx + 1, prevIndex);
+//     int foo2 = INT_MIN;
+//     if (prevIndex == -1 || numbers.at(idx) > numbers.at(prevIndex)) {
+//         foo2 = 1 + solveRecursively(idx + 1, idx);
+//     }
+//     int ans = max(foo1, foo2);
+//     if (prevIndex != -1) {
+//         dp.at(idx).at(prevIndex) = ans;
+//     }
+//     return ans;
+// }
+
+int solveIteratively() {
+    dp.resize(n + 5, -1);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (dp.at(j) >= dp.at(i)) {
+                continue;
+            }
+
+            dp.at()
+        }
     }
 
-    int foo1 = solveRecursively(idx + 1, prevIndex);
-    int foo2 = INT_MIN;
-    if (prevIndex == -1 || numbers.at(idx) > numbers.at(prevIndex)) {
-        foo2 = 1 + solveRecursively(idx + 1, idx);
-    }
-    int ans = max(foo1, foo2);
-    if (prevIndex != -1) {
-        dp.at(idx).at(prevIndex) = ans;
-    }
-    return ans;
+
 }
 
 int main() {
@@ -37,7 +53,6 @@ int main() {
         highest = max(c, highest);
         numbers.push_back(c);
     }
-    dp.resize(n + 5, vector<int>(n + 5, -1));
     cout << solveRecursively(0, -1);
     return 0;
 }
